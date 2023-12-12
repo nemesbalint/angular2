84dff49ef6c2e7026711a90016afc865a0a54739
Alerant_NemesB <balint.nemes@erstebank.hu>
2023-12-12 13:44:14 +0100

    Angular HttpClient - kapcsolat a szerverrel 1 fixup

--- myFirstAngularApp/src/app/service/football.service.ts
+++ myFirstAngularApp/src/app/service/football.service.ts
@@ -9,10 +9,10 @@ export class FootballService {
   jsonUrl: string = "https://raw.githubusercontent.com/openfootball/football.json/master/2016-17/en.1.clubs.json";
 
   constructor(private http: HttpClient) { 
-    this.http.get(this.jsonUrl).subscribe(
-      list => console.log("football list"),
-      error => console.error(error),
-      () => console.log("complete")
-    );
+    this.http.get(this.jsonUrl).subscribe({
+      next: () => console.log("football list"),
+      error: (error: any) => console.error(error),
+      complete() {console.log("complete")},
+    });
   }
 }
