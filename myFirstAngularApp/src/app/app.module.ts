588f5516e9b408017855db4a0c4ffcf2fd331a74
Alerant_NemesB <balint.nemes@erstebank.hu>
2023-12-13 10:53:57 +0100

    Angular - sablonalapú űrlapok 1

--- myFirstAngularApp/src/app/app.module.ts
+++ myFirstAngularApp/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -49,7 +50,8 @@ const appRoutes: Routes = [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [
     FootballService, 
